93a8ed89e1cfd6628afb596eb3f691af19584951
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-22 21:08:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
 function postComment() {
   var sidebar = document.getElementById("sidebar").value;
   var post = document.getElementById("post").value;
- 
+
   //insert comment into "comments" div in this format:
   //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
- 
+
   //create template string - THIS IS THE ONLY LINE WE HAVE TO CHANGE
   //var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
   var pageTemplate = document.getElementById("page-template").innerHTML;
- 
+
   //create template function
   var templateFn = _.template(pageTemplate);
- 
-  var commentsDiv = document.getElementById("comments");
- 
+
+  var commentsDiv = document.getElementById("post");
+
   //execute template function with JSON object for the interpolated values
-  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
- 
+  var templateHTML = templateFn({ 'sidebar': sidebar, 'post': post });
+
   //append rather than replace!
   commentsDiv.innerHTML += templateHTML;
 }
